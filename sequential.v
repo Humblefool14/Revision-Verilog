17a395eadac8b979e53d4ea3e07c1a21d7e0207c
Humblefool14 <32316012+Humblefool14@users.noreply.github.com>
2020-06-24 21:42:22 +0530

    Create sequential.v

---
+++ sequential.v
@@ -0,0 +1,12 @@
+module sequential(curr_state,flag);
+   input [0:1] curr_state;
+   output reg[0:1] flag;
+
+   always@(curr_state)
+       begin
+          case(curr_state)
+            0,1 : flag =2;
+            3   : flag =0;
+          endcase 
+       end
+endmodule
