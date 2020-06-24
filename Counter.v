48fbde6e3473ed86ece1ed377362f1a88255bed3
Humblefool14 <32316012+Humblefool14@users.noreply.github.com>
2020-06-24 20:59:08 +0530

    Create Counter.v

---
+++ Counter.v
@@ -0,0 +1,12 @@
+module counter(clk,rst,count);
+   input clk,rst;
+   output reg[3:0] count;
+
+   always@(posedge clk or posedge rst)
+       begin
+          if(rst)
+                count <=0;
+          else
+                count <= count+1;
+       end
+endmodule
