39eea8cb2e875cb3784efa1c41ca65e16280fda3
Humblefool14 <32316012+Humblefool14@users.noreply.github.com>
2020-06-25 13:01:41 +0530

    Create Updown counter.v

---
+++ Updown counter.v
@@ -0,0 +1,11 @@
+// Updown counter 
+module updown_counter(mode,clr,ld,d_in,clk,count);
+    input mode,clr,id,clk;
+    input [7:0] d_in;
+    output reg[0:7] count; 
+    
+    always@(posedge clk)
+        if(ld) count <= d_in;
+        else if(clr) count <=0;
+        else if(mode) count<= count+1;
+endmodule
