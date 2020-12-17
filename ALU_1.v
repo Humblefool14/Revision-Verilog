0a9caf45841259762d615152254aeeb95589af84
Humblefool14 <32316012+Humblefool14@users.noreply.github.com>
2020-12-17 15:35:19 +0530

    Update ALU_1.v

--- ALU_1.v
+++ ALU_1.v
@@ -1,1 +1,19 @@
 package definitions;
+  typedef enum{ADD,SUB,MUL,DIV,SL,SR}opcode_t;
+  typedef enum{unsigned,signed} operand_t;
+  typedef union packed{ 
+    logic [31:0]u_data;
+    logic signed [31:0]s_data;
+  } data_t;
+  typedef struct packed{
+    opcode_t opcode;
+    data_t data_a;
+    data_t data_b;
+    operand_t operand;
+  } instruction_t;
+end package;
+  
+  module(input a,input b,input c){
+    
+    
+    
