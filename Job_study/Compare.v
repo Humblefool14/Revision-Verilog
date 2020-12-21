3379daded2dea94e24d2e102d3246b00a248e7c2
Humblefool14 <32316012+Humblefool14@users.noreply.github.com>
2020-12-21 14:10:40 +0530

    Create Compare.v

---
+++ Job_study/Compare.v
@@ -0,0 +1,22 @@
+module compare(output logic lt,et,gt, 
+               input logic [31:0] a,b);
+       
+       always@(a,b)
+            if( a < b )
+                lt = 1'b1;
+            else if(a==b)
+                 lt = 1'b0;
+          
+          assign gt = (a>b);
+        
+ comparator(et,a,b);
+ endmodule 
+ 
+ module comparator (input logic[31:0] a,b,
+                    output logic et);
+         
+         if (a==b) et == 1'b1;
+ endmodule;
+ 
+            
+            
