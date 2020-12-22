407520ef29a335fff1552fcd293f2340f187ce7b
Humblefool14 <32316012+Humblefool14@users.noreply.github.com>
2020-12-22 10:57:01 +0530

    Update sensitivity lists.v

--- Job_study/sensitivity lists.v
+++ Job_study/sensitivity lists.v
@@ -4,3 +4,7 @@ always@(a,b)
     sub = a-b;
     prod = a*b;
   end 
+
+// An edge sensitive event control at the very beginning of an 
+// always procedural block is typically referred to as 
+// the sensitivity list for that procedural block
