eb56c7c1eaae584c29d2674af819ad46cc734495
Humblefool14 <32316012+Humblefool14@users.noreply.github.com>
2021-11-25 18:22:12 +0530

    Create Ref.v

---
+++ Job_study/Snippets/Ref.v
@@ -0,0 +1,5 @@
+Use ref i.e references for passing data directly. 
+Data not copied. But directly acted upon. 
+Inout is data copied. 
+
+https://stackoverflow.com/questions/31017629/what-does-ref-mean-in-systemverilog/31017968 
