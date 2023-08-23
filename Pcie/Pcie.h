4736774ebc6e8a6000d0017313cf99b6ae00e1e5
Humblefool14 <32316012+Humblefool14@users.noreply.github.com>
2023-08-23 10:11:30 +0530

    Update Header. Pcie.h

--- Pcie/Pcie.h
+++ Pcie/Pcie.h
@@ -1,1 +1,13 @@
+typedef enum pcie_device_port_typ{
+    PCI_EXPRESS_ENDPT                  = 0, 
+    LEGACY_PCI_EXPRESS_ENDPT           = 1,
+    ROOT_PORT_PCI_EXPRESS_CMPLX        = 4,
+    UPSTREAM_PORT_PCI_EXPRESS_SWITCH   = 5,
+    DOWNSTREAM_PORT_PCI_EXPRESS_SWITCH = 6,
+    PCI_EXPRESS_TO_PCI_BRIDGE          = 7,
+    PCI_TO_PCI_EXPRESS_BRIDGE          = 8,
+    RCIEP                              = 9,
+    ROOT_CMPLX_EVNT_COLLECTOR         = 10, 
+}pcie_dev_port_e; 
+#define API_OK 1
 int32_t pcie_chk_capability(void * dev); 
