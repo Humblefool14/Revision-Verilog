119d6c55fc4332617377cf64c8598cde2d6affa4
Srikar <srikar@Srikars-MacBook-Pro.local>
2023-08-28 19:36:08 +0530

    RCRB register updated

--- Pcie/Pcie_struct.h
+++ Pcie/Pcie_struct.h
@@ -5,6 +5,39 @@
 #include<stdlib.h>
 #include<stdint.h>
 #include<string.h>
+typedef union pcie_typ0_cfg_header{
+  struct __attribute__((packed)){
+    uint32_t vendor_id                   : 16; 
+    uint32_t dev_id                      : 16;
+    uint32_t command                     : 16;
+    uint32_t status                      : 16;
+    uint32_t revision_id                 : 8;
+    uint32_t class_code                  : 24;
+    uint32_t cache_line_size             : 8;
+    uint32_t primary_laten_timer         : 8;
+    uint32_t header_type                 : 8;
+    uint32_t bist_type                   : 8;
+    uint32_t bar_register_1              : 32;
+    uint32_t bar_register_2              : 32;
+    uint32_t bar_register_3              : 32;
+    uint32_t bar_register_4              : 32;
+    uint32_t bar_register_5              : 32;
+    uint32_t bar_register_6              : 32;
+    uint32_t cardbus_cis_ptr             : 32;  // hardwire to 0
+    uint32_t subsys_vendor_id            : 16;
+    uint32_t subsys_id                   : 16;
+    uint32_t expansion_rom_base          : 32; 
+    uint32_t cap_ptr                     : 8; 
+    uint32_t rsvd                        : 24; 
+    uint32_t rsvd1                       : 32; 
+    uint32_t int_line                    : 8; 
+    uint32_t int_pin                     : 8; 
+    uint32_t min_gnt                     : 8;
+    uint32_t max_lat                     : 8; 
+  };
+  uint8_t data[60];
+}pcie_typ0_cfg_header_u;
+
 typedef union pcie_typ1_cfg_header{
   struct __attribute__((packed)){
     uint32_t vendor_id                   : 16; //1+4+2+4+1+1+4+1+1+2+4+2+2+4+2+2
@@ -294,3 +327,56 @@ typedef union pcie_memwrite_request_header{
   }
     uint8_t data[8];
 }pcie_memwrite_req_u; 
+
+typedef union pcie_hierarchy_id_msg{
+  struct __attribute__((packed)){
+    uint32_t fmt                         : 3; //011
+    uint32_t type                        : 5; //10011
+    uint32_t t9                          : 1; //R
+    uint32_t tc                          : 3; //000
+    uint32_t t8                          : 1; //R
+    uint32_t attr                        : 1; //R
+    uint32_t ln                          : 1; //R
+    uint32_t th                          : 1;
+    uint32_t td                          : 1;
+    uint32_t ep                          : 1; 
+    uint32_t attr                        : 2; // RR 
+    uint32_t at                          : 2; //00
+    uint32_t length                      : 10; //4 bytes 
+    uint32_t request_id                  : 16; 
+    uint32_t rsvd                        : 8; 
+    uint32_t message_code                : 8; //01111111
+    uint32_t hierarchy_id                : 8; 
+    uint32_t vendor_id                   : 8; //1 
+    uint32_t subtype                     : 1;
+    uint32_t guid_auth_id                : 8; 
+    uint32_t system_guid_lst             : 16;     
+    uint64_t system_guid_mid             : 64;
+    uint64_t system_guid_stt             : 64; 
+  }
+    uint8_t data[28];
+}pcie_hier_idmsg_u; 
+
+typedef union pcie_rcrb_header{
+  struct __attribute__((packed)){
+    uint32_t fmt                         : 3; 
+    uint32_t type                        : 5; 
+    uint32_t t9                          : 1; 
+    uint32_t tc                          : 3; 
+    uint32_t t8                          : 1; 
+    uint32_t attr                        : 1; 
+    uint32_t ln                          : 1; 
+    uint32_t th                          : 1;
+    uint32_t td                          : 1;
+    uint32_t ep                          : 1; 
+    uint32_t attr                        : 2;  
+    uint32_t at                          : 2; 
+    uint32_t length                      : 10; 
+    uint32_t vendor_id                   : 16; 
+    uint32_t dev_id                      : 16; 
+    uint32_t rcrb_capa_reg               : 32; 
+    uint32_t rcrb_ctrl_reg               : 32; 
+    uint32_t rsvd_z                      : 32;  
+  }
+    uint8_t data[16];
+}pcie_rcrb_header_u; 
