f3c5fee3185d98fe2eaaec69e58eb1863abe0cb4
Srikar <srikar@Srikars-MacBook-Pro.local>
2023-10-03 10:58:51 +0530

    Additional updates

---
+++ Pcie/rtl/Sds.v
@@ -0,0 +1,8 @@
+//SDS 
+// 
+
+localparam [127:0] SDS_8G_identifier = {32'hE1555555,32'h55555555,32'h55555555,32'h55555555}; 
+localparam [127:0] SDS_32G_identifier = {32'hE1878787,32'h87878787,32'h87878787,32'h87878787}; 
+localparam [127:0] SDS_64G_identifier = {32'hB1C6C6C6,32'hB1C6C6C6,32'hB1C6C6C6,32'hB1C6C6C6}; 
+
+
