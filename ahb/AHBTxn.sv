0da9e5bd205e920c05ad53c8100f83c2d9b6f298
Srikar <srikar@Srikars-MBP.Dlink>
2024-04-07 13:55:13 +0530

    AHB tb sequences added

---
+++ ahb/AHBTxn.sv
@@ -0,0 +1,12 @@
+
+
+function new(string name = "AHBTXN");
+    super.new(name);
+  endfunction
+
+  function void pre_randomize();
+  endfunction
+
+  function void post_randomize();
+  endfunction 
+  
