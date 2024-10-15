da538e29617b83bef1fef9d6e7cbfd7e786228e4
Srikar <srikar@Srikars-MacBook-Pro.local>
2024-10-15 11:21:44 +0530

    prefix sums updated

--- prefix_sums/parallel_prefix_sum.cpp
+++ prefix_sums/parallel_prefix_sum.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <omp.h>
-
+#ifdef _OPENMP
+    #include <omp.h>
+#endif
 // Function to perform upscan (reduce)
 void upscan(std::vector<int>& arr) {
     int n = arr.size();
     for (int d = 0; d < std::log2(n); ++d) {
+        //for d from 0 to (lgn)−1
         int stride = std::pow(2, d + 1);
+        //in parallel for i from 0 to n−1 by 2 power(d+1) 
         #pragma omp parallel for
         for (int i = 0; i < n; i += stride) {
             if (i + stride - 1 < n) {
                 arr[i + stride - 1] += arr[i + stride / 2 - 1];
+                //a[i+pow(2,d+1) −1]←a[i+pow(2,d) −1]+a[i+pow(2,d+1) −1]
             }
         }
     }
@@ -23,13 +27,17 @@ void downscan(std::vector<int>& arr) {
     arr[n - 1] = 0;  // Set identity element
 
     for (int d = std::log2(n) - 1; d >= 0; --d) {
+        // for d from (lgn)−1 downto 0
         int stride = std::pow(2, d + 1);
         #pragma omp parallel for
         for (int i = 0; i < n; i += stride) {
             if (i + stride - 1 < n) {
                 int t = arr[i + stride / 2 - 1];
+                // t←arr[i+pow(2,d) −1] %Save in temporary
                 arr[i + stride / 2 - 1] = arr[i + stride - 1];
+                //arr[i+pow(2,d) −1]←a[i+ pow(2,d+1) −1] %Setleftchild
                 arr[i + stride - 1] += t;
+                // arr[i+pow(2,d+1) −1]←t+arr[i+pow(2,d+1) −1] %Setrightchild
             }
         }
     }
