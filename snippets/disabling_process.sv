5b0ddc78934ac1973f82c2b1fd320a1c204d57d1
Humblefool14 <32316012+Humblefool14@users.noreply.github.com>
2025-07-08 00:25:40 +0530

    Create disabling_process.sv

---
+++ snippets/disabling_process.sv
@@ -0,0 +1,37 @@
+class process;
+typedef enum { FINISHED, RUNNING, WAITING, SUSPENDED, KILLED } state;
+static function process self();
+function state status();
+function void kill();
+task await();
+function void suspend();
+function void resume();
+function void srandom( int seed );
+function string get_randstate();
+function void set_randstate( string state );
+endclass
+
+
+  task automatic do_way(int N, int k); 
+    process job[] = new[N]; 
+
+    foreach(job[i])
+      fork 
+        automatic k = j; 
+        begin job[k] = process::self(); 
+        end; 
+      join_none 
+
+    foreach(job[i])
+      wait(job[i]!=null); 
+
+    job[k].await(); 
+
+    foreach(job[i])
+      if(job[i].status != process::FINISHED)
+        job[i].kill; 
+    end 
+  endtask 
+      
+          
+    
